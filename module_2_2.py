bee23a38479c995e7957d06e8f900fa74a66b488
TaktEsy <43720303+TaktEsy@users.noreply.github.com>
2024-08-13 20:23:07 +0300

    Commit

---
+++ module_2_2.py
@@ -0,0 +1,8 @@
+first = input('Первое число: ')
+second = input('Второе число: ')
+third = input('Третье число: ')
+
+if first == second == third:
+    print(3)
+elif first != second != third:
+    print(0)
