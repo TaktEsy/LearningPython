efdf2674f661d0604bec7658df027bf2bbd38cb2
TaktEsy <43720303+TaktEsy@users.noreply.github.com>
2024-08-16 17:44:00 +0300

    Commit

--- module_2_4.py
+++ module_2_4.py
@@ -6,6 +6,7 @@
 Выведите списки primes и not_primes на экран(в консоль).
 """
 import math
+from time import sleep
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
@@ -14,24 +15,28 @@ is_prime = True
 div = 2
 
 for n in numbers:
-    if -1 <= n <= 1:
+    if n <=1:
         is_prime = False
-        continue
     else:
-        while div ** 2 <= n and is_prime is True:
-            if n % div == 0:
-                is_prime = False
-            else:
-                div += 1
-    if is_prime is True:
+        is_prime == False
         print(n)
+    # else:
+    #     for divr in range(2, int(n**0.5 + 1)):
+    #         if n % divr == 0:
+    #             is_prime = False
+    #             not_primes.append(n)
+    #             break
+    #         else:
+    #             is_prime = True
+    #             primes.append(n)
+
 
     # if n in range(2, n):
     #     print(f"Простое число: {n}")
     # if i >= 2:
     #     number_sqrt = int(math.sqrt(i))
     #     print(number_sqrt)
-
+sleep(10000000)
 print(numbers)
 print(primes)
 print(not_primes)
