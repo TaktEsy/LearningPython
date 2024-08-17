bf96e6d1acae2870ce76a909bca35ac5796ada11
TaktEsy <43720303+TaktEsy@users.noreply.github.com>
2024-08-17 18:33:26 +0300

    Commit

--- module_2_4.py
+++ module_2_4.py
@@ -1,3 +1,4 @@
+# ДЗ по уроку "Цикл for. Элементы списка. Полезные функции в цикле"
 """
 Задача "Всё не так уж просто":
 Дан список чисел numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
@@ -19,45 +20,52 @@ Not Primes: [4, 6, 8, 9, 10, 12, 14, 15]
 5. В процессе проверки на простоту записывайте числа из списка numbers в списки primes и not_primes в зависимости от значения переменной is_prime после проверки (True - в prime, False - в not_prime).
 6. Выведите списки primes и not_primes на экран(в консоль).
 """
-import math
+#
+#Задача:
+
+#--------------------------Порядок действий-------------------------------------------
+# 1. Создайте пустые списки primes и not_primes.
+# 2. При помощи цикла for переберите список numbers.
+# 3. Напишите ещё один цикл for (вложенный), где будут подбираться делители для числа из 1ого цикла.
+# 4. Отметить простоту числа можно переменной is_prime, записав в неё занчение True перед проверкой.
+# 5. В процессе проверки на простоту записывайте числа из списка numbers в списки primes и not_primes
+#    в зависимости от значения переменной is_prime после проверки (True - в prime, False - в not_prime).
+# 6. Выведите списки primes и not_primes на экран(в консоль).
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+#2. Создаю пустые списки primes и not_primes.
 primes = []
 not_primes = []
-is_prime = None
-div = 2
-
-# Цикл для перебора списка с данными
-for n in numbers:
-    # Сразу отметаем единицу т.к. она не является простым числом
-    if n != 1:
-        print(f'{n}')
-        sq = math.sqrt(n)
-        for i in range(2, sq + 1):
-            if n % i == 0:
-                not_primes.append(n)
+#2. При помощи цикла for перебираю список numbers.
+#   number - число в списке
+for number in numbers:
+    #Присваиваем флагу is_prime значение Истина
+    is_prime = True
+    #Если число number больше 1 (1 не относится ни к простым, ни к составным числам)
+    if number > 1:
+        #перебираем циклом числа от 2
+        for i in range(2, number):
+            ost=number % i
+            if ost == 0:
+               ret="Нет"
             else:
-               primes.append(n)
+               ret="Да"
+            print(f'Текущее значение:{number}    Предыдущее число с делением без остатка:{i}  Условие Простого числа: {ret}')
+            # Если остаток от деления числа равен 0, число будет составным
+            if ost == 0:
+               #присваиваем is_prime значение Ложь
+               is_prime = False
+           # иначе если число равно 1
     else:
-        print('')
-
-    # else:
-    #     for divr in range(2, int(n**0.5 + 1)):
-    #         if n % divr == 0:
-    #             is_prime = False
-    #             not_primes.append(n)
-    #             break
-    #         else:
-    #             is_prime = True
-    #             primes.append(n)
-
-
-    # if n in range(2, n):
-    #     print(f"Простое число: {n}")
-    # if i >= 2:
-    #     number_sqrt = int(math.sqrt(i))
-    #     print(number_sqrt)
-
-# print(numbers)
-# print(f'Составные числа: {primes}')
-# print(f'Простые числа: {not_primes}')
+     #переход в начало цикла
+       continue
+    # Если is_prime равно True
+    if is_prime == True:
+        #добавляем в список простых чисел число
+       primes.append(number)
+    #Иначе, is_prime равно False, добавляем число в список составных чисел
+    else:
+       not_primes.append(number)
+    #Вывод на консоль
+print(f'Простые числа:    {primes}')
+print(f'Составные числа:  {not_primes}')
