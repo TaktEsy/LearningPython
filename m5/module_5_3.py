67d9abf743081d445f0a494d496f3f9effbe7233
TaktEsy <43720303+TaktEsy@users.noreply.github.com>
2024-09-12 21:12:19 +0300

    Commit

--- m5/module_5_3.py
+++ m5/module_5_3.py
@@ -1,14 +1,14 @@
 class House:
-    def __init__(self, name, number_of_floors ):
+    def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
     def __len__(self):
         return self.number_of_floors
     def __str__(self):
-        return (f"Название {self.name}; Этажность: {self.number_of_floors}")
+        return (f"Название: {self.name}; Этажность: {self.number_of_floors}")
 
     def __eq__(self, other):
-        pass
+        return self.number_of_floors == other
 
     def __lt__(self, other):
         pass
@@ -25,13 +25,15 @@ class House:
         pass
 
     def __add__(self, other):
-        pass
+        if not isinstance(self.number_of_floors, int) or not isinstance(other, int):
+            raise TypeError("Этажи должны быть целым числом!")
+        return self.number_of_floors + other
 
     def __radd__(self, other):
         pass
 
     def __iadd__(self, other):
-        pass
+        return self.__add__(self, other)
 
     def go_to(self, new_floor):
         if new_floor > self.number_of_floors or new_floor < 1:
@@ -42,5 +44,21 @@ class House:
                 print(f'Этаж: {i}')
 
 
-h2 = House('Бурдж-Халифа', 163)
+h1 = House('Бурдж-Халифа', 163)
 h2 = House('Куала-Лумпур', 118)
+
+print(h1)
+print(h2)
+
+# __eq__
+print(h1 == h2)
+
+# __add__
+h2 = h2 + 45
+print(h1)
+print(h2)
+print(h1 == h2)
+
+# __iadd__
+h2 += 45
+print(h2)
