1eee05a1bf257953b792a65cb74bc09c241a9cbe
TaktEsy <43720303+TaktEsy@users.noreply.github.com>
2024-09-25 17:45:28 +0300

    Практическая работа ч.3

--- m5/module_5_prac1.py
+++ m5/module_5_prac1.py
@@ -19,20 +19,35 @@ class User:
 
 if __name__ == '__main__':
     db = Database()
-    user = User(input("Login: "), password := input("Password: "), password_confirm := input("Repeat password: "))
 
+    while True:
+        choice = int(input('Выберите действие: \n1 - Вход\n2 - Регистрация\n'))
+        if choice == 1:
+            login = input("Логин: ")
+            password = input("Пароль: ")
+            if login in db.data:
+                if password == db.data[login]:
+                    print(f"Добро пожаловать, {login}")
+                    break
+                else:
+                    print("Пароль неверный!")
+            else:
+                print("Пользователь не найден!")
+        elif choice == 2:
+            user = User(input("Login: "), password := input("Password: "),
+                        password_confirm := input("Repeat password: "))
 
-    if password != password_confirm:
-        exit('Пароли не совпадают!')
-    elif len(password) < 2:
-        exit('Символов должно быть больше!')
-    else:
-        counter = 0
-        for s in password:
-            if s.isupper():
-                counter += 1
-        if counter == 0:
-            exit('Нет заглавной!')
-    db.add_user(user.username, user.password)
+            if password != password_confirm:
+                exit('Пароли не совпадают!')
+            elif len(password) < 2:
+                exit('Символов должно быть больше!')
+            else:
+                counter = 0
+                for s in password:
+                    if s.isupper():
+                        counter += 1
+                if counter == 0:
+                    exit('Нет заглавной!')
+            db.add_user(user.username, user.password)
 
-    print(db.data)
+        print(db.data)
