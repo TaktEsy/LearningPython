2c2b192118b972f05415db8613cb3a7a76760a5b
TaktEsy <43720303+TaktEsy@users.noreply.github.com>
2024-10-03 17:51:19 +0300

    - Imported and repaired "arcade" - Background  is white

--- m6/ping-pong/game.py
+++ m6/ping-pong/game.py
@@ -1,1 +1,10 @@
-print("Hello")
+import arcade
+
+
+class Game(arcade.Window):
+    def on_draw(self) -> bool | None:
+        self.clear((255, 255, 255))
+
+if __name__ == '__main__':
+    window =  Game()
+    arcade.run()
